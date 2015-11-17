7893f0c0a356d827a09e2146e4d04b4faf4f0759
Giuseppe Luigi <joseluis.canovas@outlook.com>
2015-11-17 16:14:44 +0100

    Installation scripts organizados

--- INSTALL/install.sh
+++ INSTALL/install.sh
@@ -1,28 +1,20 @@
 #!/bin/bash
 apt-get -y install quagga
 
-cat > /etc/quagga/daemons << EOF
-zebra=yes
-bgpd=no
-ospfd=no
-ospf6d=yes
-ripd=no
-ripngd=no
-isisd=no
-babeld=no
-
-EOF
-
 
 touch /etc/quagga/zebra.conf
+touch /etc/quagga/bgpd.conf
 touch /etc/quagga/ospf6d.conf
+touch /etc/quagga/ripngd.conf
 touch /etc/quagga/vtysh.conf
 
 chown quagga.quaggavty /etc/quagga/*.conf
 chmod 640 /etc/quagga/*.conf
 
 echo 'password manyhue' > /etc/quagga/zebra.conf
+echo 'password manyhue' > /etc/quagga/bgpd.conf
 echo 'password manyhue' > /etc/quagga/ospf6d.conf
+echo 'password manyhue' > /etc/quagga/ripngd.conf
 echo 'username root nopassword' > /etc/quagga/vtysh.conf
 
 
