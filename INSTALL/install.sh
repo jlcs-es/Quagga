69e42183968116cb7240dd8f12a14f67080e52cb
Giuseppe Luigi <joseluis.canovas@outlook.com>
2015-11-24 14:19:09 +0100

    Fix RA autoconf de los routers.

--- INSTALL/install.sh
+++ INSTALL/install.sh
@@ -18,12 +18,14 @@ echo 'password manyhue' > /etc/quagga/ripngd.conf
 echo 'username root nopassword' > /etc/quagga/vtysh.conf
 
 
-sysctl -w net.ipv6.conf.all.forwarding=1
+sysctl -w net.ipv6.conf.all.autoconf = 0
+sysctl -w net.ipv6.conf.all.accept_ra = 0
+sysctl -w net.ipv6.conf.all.accept_redirects = 0
+sysctl -w net.ipv6.conf.all.forwarding = 1
+sysctl -w net.ipv6.conf.all.router_solicitations = 0
 
 echo "*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*"
 echo "Recuerde hacer en cada reinicio:"
-echo "   sysctl -w net.ipv6.conf.all.forwarding=1   "
-echo "O bien:"
 echo "   sudo make ipv6forward   "
 echo "*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*"
 sleep 3
